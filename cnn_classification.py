5287b4323cf4e8f132471bbc66c6eff4f21be7b2
Purushottam Chaudhary <roshan.purush@gmail.com>
2021-06-23 06:41:51 +0530

    predict all test images

--- cnn_classification.py
+++ cnn_classification.py
@@ -139,9 +139,9 @@ for img in range(500):
     dog_img = load_image(dog_img_path)
 
     print('cat : ' + str(model.predict(cat_img)) + ' : correctly predicted :-)' if model.predict(
-        cat_img) < 5.0 else ' : Failed')
+        cat_img) < 0.5 else ' : Failed')
     print('dog : ' + str(model.predict(dog_img)) + ' : correctly predicted :-)' if model.predict(
-        dog_img) > 5.0 else ' : Failed')
+        dog_img) > 0.5 else ' : Failed')
 
 # Optional - For Development and Visualisation purposes
 # visualize()
